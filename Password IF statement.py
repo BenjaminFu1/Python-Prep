a5da637d5b7e738d3c2dcf280ef3cf95820947e7
Ben Fu <fube2015@cranleigh.org>
2016-11-07 09:47:12 +0000

    Add files via upload
    There is my If statement preps

---
+++ Password IF statement.py
@@ -0,0 +1,6 @@
+password=input("Please enter a password")
+repassword=input("Please re-enter the password")
+if password==(repassword):
+    print("Access granted")
+else:
+    print("Access denied")
---
+++ Pedestrian crossing IF statement.py
@@ -0,0 +1,5 @@
+lightcolour=input("Please enter the colour of the light at the pedestrian crossing (Green or Red)")
+if lightcolour == "Green":
+    print("It is safe to cross")
+else:
+    print("Stop!!!")
