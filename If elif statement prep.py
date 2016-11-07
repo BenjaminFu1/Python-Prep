0d89b7e9d683d17b2ca1207b5c874282aa5e98de
Ben Fu <fube2015@cranleigh.org>
2016-11-07 19:10:26 +0000

    Add files via upload
    Week 10: Selection Statements & Input/Process/Output Model (Task 1-2)

---
+++ If elif statement prep.py
@@ -0,0 +1,14 @@
+import random
+answer= random.randint(1,6)
+if answer == 1:
+        print("You will make a new friend this week")
+elif answer == 2:
+        print("You will do well in your GCSEs")
+elif answer == 3:
+        print("You will find something you thought you'd lost")
+elif answer == 4:
+        print("You will get a credit this week")
+elif answer == 5:
+        print("You will get a girl friend this month")
+elif answer == 6:
+        print("You will find a 50 pounds note on the floor this month")
