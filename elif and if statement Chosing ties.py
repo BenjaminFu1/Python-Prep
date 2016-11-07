be1605a04835429dc6154654d745fd04e988cec6
Ben Fu <fube2015@cranleigh.org>
2016-11-07 19:26:55 +0000

    Add files via upload
    This program helps you choosing ties

---
+++ elif and if statement Chosing ties.py
@@ -0,0 +1,14 @@
+import random
+colour= random.randint(1,6)
+if colour == 1:
+        print("Today, you should wear red coloured tie")
+elif colour == 2:
+        print("Today, you should wear orange coloured tie")
+elif colour == 3:
+        print("Today, you should wear yellow coloured tie")
+elif colour == 4:
+        print("Today, you should wear green coloured tie")
+elif colour == 5:
+        print("Today, you should wear blue coloured tie")
+elif colour == 6:
+        print("Today, you should wear purple coloured tie")
