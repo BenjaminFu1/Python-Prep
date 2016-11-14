7595085ea4ec7bb0c34f1deb10fb41c1001c2be4
Ben Fu <fube2015@cranleigh.org>
2016-11-14 10:18:30 +0000

    Update If elif statement prep.py

--- If elif statement prep.py
+++ If elif statement prep.py
@@ -1,5 +1,11 @@
+#This is my fortune telling program
 import random
+import time
 answer= random.randint(1,6)
+print("\nWelcome to my fortune telling program\n")
+print("I am going to choose a number between 1-6 for you")
+time.sleep(3)
+print("Your number is:",answer"\n")
 if answer == 1:
         print("You will make a new friend this week")
 elif answer == 2:
@@ -10,5 +16,5 @@ elif answer == 4:
         print("You will get a credit this week")
 elif answer == 5:
         print("You will get a girl friend this month")
-elif answer == 6:
+else:
         print("You will find a 50 pounds note on the floor this month")
